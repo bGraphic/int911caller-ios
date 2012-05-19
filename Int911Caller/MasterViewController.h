a4f35e68baa50dae7cd90e6de9adcd793ee47017
raae <raae@bgraphic.no>
2012-05-19 18:38:07 +0200

    Initial Commit

---
+++ Int911Caller/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  Int911Caller
+//
+//  Created by Benedicte Raae on 19.05.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
