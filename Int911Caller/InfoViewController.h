7606adcb0efe6f769d4d1bdb741cb9cff0c6f4b5
raae <raae@bgraphic.no>
2012-06-07 14:30:25 +0200

    Share Kit added

--- Int911Caller/InfoViewController.h
+++ Int911Caller/InfoViewController.h
@@ -7,7 +7,25 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MessageUI/MFMailComposeViewController.h>
 
-@interface InfoViewController : UIViewController
+@interface InfoViewController : UIViewController <MFMailComposeViewControllerDelegate>
+
+@property (strong, nonatomic) NSDictionary *infoDict;
+
+@property (strong, nonatomic) IBOutlet UIButton *rateAppButton;
+@property (strong, nonatomic) IBOutlet UIButton *shareButton;
+@property (strong, nonatomic) IBOutlet UIButton *supportButton;
+
+@property (strong, nonatomic) IBOutlet UIButton *blogButton;
+@property (strong, nonatomic) IBOutlet UIButton *moreAppsButton;
+
+- (IBAction)rateAppAction:(id)sender;
+- (IBAction)shareAction:(id)sender;
+- (IBAction)supportAction:(id)sender;
+- (IBAction)blogAction:(id)sender;
+- (IBAction)moreAppsAction:(id)sender;
+
+- (IBAction)closeInfoAction:(id)sender;
 
 @end
--- Int911Caller/InfoViewController.m
+++ Int911Caller/InfoViewController.m
@@ -9,28 +9,34 @@
 #import "InfoViewController.h"
 
 @interface InfoViewController ()
-
+-(void)openUrl:(NSString *)urlString;
 @end
 
 @implementation InfoViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
+@synthesize infoDict = _infoDict;
+@synthesize rateAppButton = _rateAppButton;
+@synthesize shareButton = _shareButton;
+@synthesize supportButton = _supportButton;
+@synthesize blogButton = _blogButton;
+@synthesize moreAppsButton = _moreAppsButton;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-}
 
+    self.infoDict = [[NSDictionary alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"InfoPage" ofType:@"plist"]];
+}
+    
 - (void)viewDidUnload
 {
+    [self setRateAppButton:nil];
+    [self setShareButton:nil];
+    [self setSupportButton:nil];
+    [self setBlogButton:nil];
+    [self setMoreAppsButton:nil];
+
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -39,5 +45,96 @@
 {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
+
+- (IBAction)rateAppAction:(id)sender {
+    NSString *urlString = [NSString stringWithFormat:@"itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=%@", [self.infoDict objectForKey:@"app_id"]];
+    
+    [self openUrl:urlString];
+}
+
+- (IBAction)shareAction:(id)sender {
+    NSString *shareLink = [NSString stringWithFormat:@"http://itunes.apple.com/app/id%@", [self.infoDict objectForKey:@"app_id"]];
+    NSString *body = [NSString stringWithFormat:NSLocalizedString(@"share_mail_body", nil), shareLink]; 
+    
+    [self sendMailTo:nil withTitle:NSLocalizedString(@"share_mail_title", nil) andBody:body];
+}
+
+- (IBAction)supportAction:(id)sender {
+    [self sendMailTo:[self.infoDict objectForKey:@"support_mail"] withTitle:NSLocalizedString(@"support_mail_title", nil) andBody:nil];
+}
+
+- (IBAction)blogAction:(id)sender {
+    [self openUrl:[self.infoDict objectForKey:@"blog_link"]];
+}
+
+- (IBAction)moreAppsAction:(id)sender {
+    NSString *urlString = [NSString stringWithFormat:@"itms-apps://itunes.com/apps/%@", [self.infoDict objectForKey:@"store_id"]];
+
+    [self openUrl:urlString];    
+}
+
+- (IBAction)closeInfoAction:(id)sender {
+    [self dismissModalViewControllerAnimated:TRUE];
+}
+
+-(void)openUrl:(NSString *)urlString {
+    UIApplication *appDelegate = [UIApplication sharedApplication];
+    NSURL *url = [[NSURL alloc] initWithString:urlString];
+    
+    if ([appDelegate openURL:url]) {
+        NSLog(@"%@%@",@"Opened url:", [url description]);
+    } 
+}
+
+-(void)sendMailTo:(NSString *)recipient withTitle:(NSString *)title andBody:(NSString *)body  {
+    MFMailComposeViewController *picker = [[MFMailComposeViewController alloc] init];
+	
+    picker.mailComposeDelegate = self;
+    
+    if(recipient != nil) {
+        [picker setToRecipients:[[NSArray alloc] initWithObjects:recipient, nil]];
+    }
+    
+    if(title != nil) {
+        [picker setSubject:title];
+    }
+    
+    if(body != nil) {
+    	[picker setMessageBody:body isHTML:YES];
+    }
+	
+
+	
+	[self presentViewController:picker animated:true completion:^{
+        NSLog(@"Mail composer has been presented.");
+    }];
+    
+    picker = nil;
+}
+
+// Dismisses the email composition interface when users tap Cancel or Send. Proceeds to update the message field with the result of the operation.
+- (void)mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error 
+{	
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"e-mail_error_alert_title", nil) message:NSLocalizedString(@"e-mail_error_alert_message", nil) delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    
+	// Notifies users about errors associated with the interface
+	switch (result)
+	{
+		case MFMailComposeResultCancelled:
+            [controller dismissModalViewControllerAnimated:YES];
+			break;
+		case MFMailComposeResultSaved:
+            [controller dismissModalViewControllerAnimated:YES];
+			break;
+		case MFMailComposeResultSent:
+            [controller dismissModalViewControllerAnimated:YES];
+			break;
+		default:
+            [alert show];
+			break;
+	}
+
+}
+
 
 @end
